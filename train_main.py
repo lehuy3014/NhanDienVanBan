d9ce07a1eccf03be7527122285e36a7aea6c2514
lehuy3014 <huyle3012004@gmail.com>
2025-10-26 16:39:00 +0700

    Update train_main.py

--- train_main.py
+++ train_main.py
@@ -15,7 +15,7 @@ from model_cnn_transformer import OCRModel
 # --- Hyperparameters ---
 BATCH_SIZE = 32  # Giảm batch size cho ConvNeXt-Large
 ACCUMULATION_STEPS = 2  # Gradient accumulation để tăng effective batch size
-EPOCHS = 5
+EPOCHS = 10
 LEARNING_RATE = 5e-5  # Giảm learning rate cho ConvNeXt-Large
 WARMUP_EPOCHS = 3  # Warmup epochs
 WEIGHT_DECAY = 1e-4  # Weight decay cho AdamW
